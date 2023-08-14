e5200c5691c97caa34cdf01647bf68dbfd8be13c
Ruslan Sakhautdinov <sakhrus1406199@gmail.com>
2023-08-14 13:33:06 +0700

    filtered array

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ Vue.createApp({
     },
     items:[1,2,3,4,5,6]
      }),
-    //  methods: {
-    //     stopPropagation(event){
-    //     event.stopPropagation()
-    // }
-    //  },
+    computed:{
+    evenItems(){
+        return this.items.filter(i=> i %2===0)
+    }
+    }
 }).mount('#app')
