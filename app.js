e2c117219855e0cb728925717c99a25be610663a
Ruslan Sakhautdinov <sakhrus1406199@gmail.com>
2023-08-14 13:49:36 +0700

    final

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ Vue.createApp({
     },
     items:[1,2,3,4,5,6]
      }),
-    computed:{
+    methods: {
+        addItem(){
+            this.items.unshift(this.$refs.myInput)
+            this.$refs.myInput.value=''
+        }
+    },
+     computed:{
     evenItems(){
         return this.items.filter(i=> i %2===0)
     }
